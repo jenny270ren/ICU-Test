20890511f114d3cb8917488e109bf00c46ae4019
jenny270ren <sijin.ren21@ycdsbk12.ca>
2020-03-23 11:24:30 -0400

    login

---
+++ Lesson1/Lesson1.py
@@ -0,0 +1,18 @@
+
+#Data Types
+word = "string"
+number = 5 #Integer
+n = 1.2 #float
+
+#boolean
+switch = True
+
+#basic operators (+-*/)
+
+#convert datatypes int(float/String)/float()/str()
+
+#concatenation - combine strings
+
+
+print("concaString")
+
